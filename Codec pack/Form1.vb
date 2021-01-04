9acb5a573d12931243fd156ae345b343aab85c88
Alexandr <mail@nomail.com>
2021-01-05 00:31:32 +0300

    upload proj

---
+++ Codec pack/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+    Private Sub Form1_Shown(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Shown
+        Timer1.Enabled = True
+    End Sub
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Me.Close()
+    End Sub
+End Class
